5aa628b32db7c313f1b898f708a153690fbfae58
Oksana860 <maroksana96@gmail.com>
2024-07-18 17:39:30 -0500

    Save changes

---
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/DashboardPage.java
@@ -0,0 +1,27 @@
+package com.chromatech.Cucumber_BDD_Testing.pages;
+
+import com.chromatech.utils.WebDriverUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+
+    public DashboardPage() {
+        PageFactory.initElements(WebDriverUtils.driver, this);
+    }
+
+    // Student Information Module
+    @FindBy(xpath = "//span[normalize-space()='Student Information']")
+    public WebElement studentInformationModule;
+
+    // Student Admission SubModule
+    @FindBy(xpath = "//a[normalize-space()='Student Admission']")
+    public WebElement studentAdmissionSubModule;
+
+    // Homework Module
+    @FindBy(xpath = "//li[@class='treeview']//a[@href='#']")
+    public WebElement homeworkModule;
+
+
+}
