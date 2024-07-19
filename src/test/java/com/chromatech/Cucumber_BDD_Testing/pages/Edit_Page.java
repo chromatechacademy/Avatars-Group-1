f76a8e37e90c58f78afca65492da2e8fefd13ae2
antonyv15 <antonyvalle15@gmail.com>
2024-07-19 13:22:07 -0400

    new changes

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/Edit_Page.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/Edit_Page.java
@@ -16,6 +16,34 @@ public class Edit_Page {
     @FindBy(xpath = "//h3[normalize-space()='Edit Student']")
     public WebElement editStudentHeader;
 
+    //Admission No
+    @FindBy(xpath = "//input[@id='admission_no']")
+    public WebElement admissionNo;
 
+    //Roll Number
+    @FindBy(xpath = "//input[@id='roll_no']")
+    public WebElement rollNumber;
+
+    //First Name
+    @FindBy(xpath = "//input[@id='firstname']")
+    public WebElement firstName;
+
+    //Last Name
+    @FindBy(xpath = "//input[@id='lastname']")
+    public WebElement lastName;
+
+    //Email
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement email;
+
+    //Mobile Number
+    @FindBy(css = "input[id='custom_fields[students][1]']")
+    public WebElement mobileNumber;
+
+    //Height
+    @FindBy(css = "input[id='custom_fields[students][2]']")
+    public WebElement height;
+
+    //
 
 }
