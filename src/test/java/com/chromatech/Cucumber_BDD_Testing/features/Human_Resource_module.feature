f885e17acebeab641f95f5704dbc361243532233
Demetriy2024 <dmytrocdl2023.gmail.com>
2024-07-19 13:26:27 -0700

    Yes

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/features/Human_Resource_module.feature
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/features/Human_Resource_module.feature
@@ -2,6 +2,6 @@ Feature: HumanResourseModule
 
   @Regression @Dmytro @AG1CP-9 @homeResource
   Scenario: HumanResourseModule
-    Given a CTSMS user is on the dashboard page
-    When the user clicks on the Human Resource module
-    Then the following submodules are displayed
+    When a CTSMS user is on the dashboard page
+   // When the user clicks on the Human Resource module
+   // Then the following submodules are displayed
--- src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Human_Resource_module_steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Human_Resource_module_steps.java
@@ -1,4 +1,15 @@
 package com.chromatech.Cucumber_BDD_Testing.stepDefinitions;
 
+import com.chromatech.Cucumber_BDD_Testing.pages.DashboardPage;
+import com.chromatech.utils.CommonMethods;
+import io.cucumber.java.en.When;
+
 public class Human_Resource_module_steps {
-}
+
+    DashboardPage DashboardPage = new DashboardPage();
+
+    @When("CTSMS user is on the dashboard page")
+    public void the_user_clicks_on_the_Human_Resource_module() {
+        CommonMethods.assertTrue(DashboardPage.StudentHomeworkModule.isDisplayed());
+        CommonMethods.click(DashboardPage.StudentHomeworkModule);
+    }
