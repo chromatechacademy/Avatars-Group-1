3ed4d49712f089b061fd19525de73fcdddf4d2a9
Vlad <vladyslavvakhutko@gmail.com>
2024-07-19 10:13:13 -0700

    Update Security_Mechanism_ForInvalid_Credentials_Login_steps.java
    +

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Security_Mechanism_ForInvalid_Credentials_Login_steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Security_Mechanism_ForInvalid_Credentials_Login_steps.java
@@ -4,8 +4,6 @@ import com.chromatech.Cucumber_BDD_Testing.pages.LoginPage;
 import com.chromatech.utils.CommonMethods;
 import io.cucumber.java.en.Then;
 import org.testng.Assert;
-
-
 import static com.chromatech.utils.WebDriverUtils.driver;
 
 public class Security_Mechanism_ForInvalid_Credentials_Login_steps {
@@ -21,7 +19,4 @@ public class Security_Mechanism_ForInvalid_Credentials_Login_steps {
     public void an_error_message_is_displayed() {
       CommonMethods.assertTrue(loginPage.errorMessage.isDisplayed());
     }
-    }
-
-
-
+}
