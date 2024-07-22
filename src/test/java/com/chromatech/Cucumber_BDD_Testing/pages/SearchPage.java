ffba8ab1e84f052b607e98958b91d7bcdb2c8fc8
Miriad42 <vladyslavvakhutko@gmail.com>
2024-07-22 16:52:06 -0700

    Vlad

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/SearchPage.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/SearchPage.java
@@ -2,11 +2,11 @@ package com.chromatech.Cucumber_BDD_Testing.pages;
 
 import com.chromatech.utils.CommonMethods;
 import io.cucumber.datatable.DataTable;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
